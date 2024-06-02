a871d155d8eef7d433fc6976f013d83f597654cd
tusharkumar2302 <tusharkumar27864@gmail.com>
2024-06-02 07:01:20 +0530

    new screen added

---
+++ src/screen/PlayerScreen.jsx
@@ -0,0 +1,67 @@
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import {colors} from '../constants/colors';
+import {iconSizes} from '../constants/dimensions';
+import {spacing} from '../constants/dimensions';
+import {fontFamilies} from '../constants/fonts';
+import {fontSize} from '../constants/dimensions';
+import {Image} from 'react-native';
+
+// icons
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const imgUrl =
+  'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/287/325x325/mortals-feat-laura-brehm-1586948734-yFnA6l5Geq.jpg';
+const PlayerScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <TouchableOpacity>
+          <AntDesign
+            name={'arrowleft'}
+            color={colors.iconPrimary}
+            size={iconSizes.lg}
+          />
+        </TouchableOpacity>
+        <Text style={styles.headerText}>Playing Now</Text>
+      </View>
+      <View style={styles.coverImageContainer}>
+        <Image source={{uri: imgUrl}} style={styles.coverImage} />
+      </View>
+    </View>
+  );
+};
+
+export default PlayerScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    paddingHorizontal: spacing.lg,
+    paddingTop: spacing.lg,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+  },
+  headerText: {
+    color: colors.textPrimary,
+    justifyContent: 'center',
+    fontSize: fontSize.lg,
+    fontFamily: fontFamilies.medium,
+    flex: 1,
+    textAlign: 'center',
+  },
+  coverImage: {
+    height: 300,
+    width: 300,
+    borderRadius: 10,
+  },
+  coverImageContainer:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: spacing.xl,
+  }
+});
