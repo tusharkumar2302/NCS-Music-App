b22bc38d2fbdafb5bb8f2836e6d3f69bb44734c2
tusharkumar2302 <tusharkumar27864@gmail.com>
2024-06-02 06:59:52 +0530

    new screens added

--- App.jsx
+++ App.jsx
@@ -3,14 +3,21 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './src/screen/HomeScreen';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import LikeScreen from './src/screen/LikeScreen';
+import PlayerScreen from './src/screen/PlayerScreen';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Navigator
+          screenOptions={{headerShown: false}}
+          initialRouteName="PLAYER_SCREEN"
+          >
           <Stack.Screen name="HOME_SCREEN" component={HomeScreen} />
+          <Stack.Screen name="LIKE_SCREEN" component={LikeScreen} />
+          <Stack.Screen name="PLAYER_SCREEN" component={PlayerScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </GestureHandlerRootView>
@@ -18,5 +25,3 @@ const App = () => {
 };
 
 export default App;
-
-// const styles = StyleSheet.create({});
