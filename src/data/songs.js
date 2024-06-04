72240c92bed37a437495d647299ea004d8868868
tusharkumar2302 <tusharkumar27864@gmail.com>
2024-06-04 22:54:57 +0530

    music data

---
+++ src/data/songs.js
@@ -0,0 +1,23 @@
+export const recommendedSongs = [
+      {
+        url: 'https://qiyvibqskjzeujclndzs.supabase.co/storage/v1/object/public/ncs/Cafe_Disko___Ella_Rosa_-_Red_Lights__NCS_Release_.mp3',
+        title: 'Red Lights',
+        artist: 'Cafe Disko, Ella Rosa',
+        artwork:
+          'https://linkstorage.linkfire.com/medialinks/images/2481b6ef-c5e4-47a6-a5c9-214f9a1f5e68/artwork-440x440.jpg',
+      },
+      {
+        url: 'https://qiyvibqskjzeujclndzs.supabase.co/storage/v1/object/public/ncs/Sub_Urban_-_Cradles__NCS_Release_.mp3',
+        title: 'Cradles',
+        artist: 'Sub Urban',
+        artwork:
+          'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/577/325x325/1714555301_9XwoUbAlJy_artwork-Cradles.png',
+      },
+      {
+        url: 'https://qiyvibqskjzeujclndzs.supabase.co/storage/v1/object/public/ncs/Warriyo_-_Mortals__feat._Laura_Brehm___NCS_Release_.mp3',
+        title: 'Mortals (feat.Laura Brehm)',
+        artist: 'Warriyo, Laura Brehm',
+        artwork:
+          'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/287/325x325/mortals-feat-laura-brehm-1586948734-yFnA6l5Geq.jpg',
+      },
+    ],
---
+++ src/data/songsWithCategory.js
@@ -0,0 +1,8 @@
+export const songsWithCategory = [
+  {
+    title: 'Recommended for you',
+    songs: [
+     
+    ],
+  },
+];
