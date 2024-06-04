9dd821a1e7257892318d2a6909dcb3db6fcd7819
tusharkumar2302 <tusharkumar27864@gmail.com>
2024-06-04 22:54:39 +0530

    made some routing changes

--- App.jsx
+++ App.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from './src/screen/HomeScreen';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import LikeScreen from './src/screen/LikeScreen';
-import PlayerScreen from './src/screen/PlayerScreen';
+import StackNavigation from './src/navigation/StackNavigation';
+import DrawerNavigator from './src/navigation/DrawerNavigator';
 
-const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{headerShown: false}}
-          initialRouteName="PLAYER_SCREEN">
-          <Stack.Screen name="HOME_SCREEN" component={HomeScreen} />
-          <Stack.Screen name="LIKE_SCREEN" component={LikeScreen} />
-          <Stack.Screen name="PLAYER_SCREEN" component={PlayerScreen} />
-        </Stack.Navigator>
+        {/* <StackNavigation /> */}
+        <DrawerNavigator />
       </NavigationContainer>
     </GestureHandlerRootView>
   );
