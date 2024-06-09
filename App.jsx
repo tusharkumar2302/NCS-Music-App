4dac587a23eb2094a6a4bffcb79df6d9979187b3
tusharkumar2302 <tusharkumar27864@gmail.com>
2024-06-09 10:29:42 +0530

    track player setup

--- App.jsx
+++ App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import StackNavigation from './src/navigation/StackNavigation';
 import DrawerNavigator from './src/navigation/DrawerNavigator';
+import {useSetupPlayer} from './src/hooks/useSetupTrackPlayer';
 
 const App = () => {
+  // track player setup
+  const onLoad = () => {
+    // console.log('track player setup...');
+  };
+  useSetupPlayer({onLoad});
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <NavigationContainer>
