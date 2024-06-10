a810f28b2bde1822ca74f9f16b6b2bc430378977
tusharkumar2302 <tusharkumar27864@gmail.com>
2024-06-10 21:53:51 +0530

    added functionality -> shuffle toggle

--- src/components/PlayerShuffleToggle.jsx
+++ src/components/PlayerShuffleToggle.jsx
@@ -3,10 +3,18 @@ import React from 'react';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {iconSizes} from '../constants/dimensions';
 import {colors} from '../constants/colors';
+import TrackPlayer, {TrackType} from 'react-native-track-player';
 
 const PlayerShuffleToggle = () => {
+  const shuffleSongs = async () => {
+    let queue = await TrackPlayer.getQueue();
+    await TrackPlayer.reset();
+    queue.sort(() => Math.random() - 0.5);
+    await TrackPlayer.add(queue);
+    await TrackPlayer.play();
+  };
   return (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={shuffleSongs}>
       <MaterialCommunityIcons
         name={'shuffle'}
         size={iconSizes.lg}
@@ -18,4 +26,4 @@ const PlayerShuffleToggle = () => {
 
 export default PlayerShuffleToggle;
 
-const styles = StyleSheet.create({});
+
