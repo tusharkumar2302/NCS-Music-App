a2d07ed72e9f53993a40b09d14f3efc91b6ad084
tusharkumar2302 <tusharkumar27864@gmail.com>
2024-06-16 03:21:27 +0530

    like song list added

--- App.jsx
+++ App.jsx
@@ -3,11 +3,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import DrawerNavigator from './src/navigation/DrawerNavigator';
 import {useSetupPlayer} from './src/hooks/useSetupTrackPlayer';
+import useLikeSongs from './src/store/likeStore';
 
 const App = () => {
+  const {loadLikeSongs} = useLikeSongs();
   // track player setup
   const onLoad = () => {
     // console.log('track player setup...');
+    loadLikeSongs();
   };
   useSetupPlayer({onLoad});
   return (
